2bfec93b2e499dc0cde2f80cc99722ae095f5d18
uriyaharel <uriyaharel@uriya>
2015-07-28 13:24:38 +0300

    new package of uria

---
+++ Eclipse Git Check4/src/uria/Hello.java
@@ -0,0 +1,10 @@
+package uria;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("hello new  brave world !!");
+	}
+
+}
